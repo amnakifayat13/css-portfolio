cb0743ab3c5856159ac8f89943f1b87183607584
amnajee-art <aftabamna13@gmail.com>
2024-10-27 20:38:20 +0500

    editing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,16 +18,16 @@ function About(){
                     <div>
 
                         <p className="get">Get to know me!</p>
-                        <p className="about-para"> I'm a passionate web developer dedicated to crafting<br/>
+                        <p className="about-para"> I &apos;m a passionate web developer dedicated to crafting<br/>
                              high-quality, engaging digital experiences. With a <br/>
                              strong foundation in front-end and back-end technologies,<br/>
                               I enjoy building responsive and interactive websites that<br/>
                                blend creativity with functionality. From user-friendly<br/>
                                interfaces to robust server-side applications, I focus<br/>
                                 on delivering solutions that not only meet user needs <br/>
-                                but also enhance business goals. I’m constantly learning<br/>
+                                but also enhance business goals. I&apos;m constantly learning<br/>
                                  and experimenting with the latest web technologies,<br/> 
-                                 ensuring my skills stay at the cutting edge. Let’s<br/>
+                                 ensuring my skills stay at the cutting edge. Let&apos;s<br/>
                                   work together to bring your vision to life on the web!</p>
                     </div>
                 </div>
