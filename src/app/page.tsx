0bd67b0ba0dd499dd7913cd29babaa720e8bfb42
amnajee-art <aftabamna13@gmail.com>
2024-10-29 11:09:50 +0500

    edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
 
 import Link from "next/link"
 import Intro from "./intro/page";
-import About from "./about/page";
-import Projects from "./projects/page"
-import Contact from "./contact/page"
-
 
 
 export default function Home() {
   return (
    <div>
     <Intro/>
-    <About/>
-    <Projects/>
-    <Contact/>
+    
+    
    </div>
   );
 }
